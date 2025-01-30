a1b8785bfe71d9d288c3b5e01cf805fed299c080
Nakka <waste.of.talent.nakka@gmail.com>
2025-01-31 00:38:58 +0900

    tenichi test map page

---
+++ tenichi_rust/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.test1to2').on('click', function () {
+    // $('.team01').addClass('team02').css({
+    //   opacity: .5
+    // });
+    $('.team01').addClass('team02');
+    $('.team01').removeClass('team01');
+    // $('.team02').fadeIn(1000);
+  })
+});
