c45eeed53faf749fee01eb53b7c48d9bba5dac02
Nakka <waste.of.talent.nakka@gmail.com>
2025-03-15 00:22:06 +0900

    [update]OBS用のスクリプト追加

--- tenichi_rust/js/script.js
+++ tenichi_rust/js/script.js
@@ -7,4 +7,25 @@ $(function () {
     $('.team01').removeClass('team01');
     // $('.team02').fadeIn(1000);
   })
+
+  // param for obs
+  function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+  var param = location.search;
+  console.log(param)
+  if (getParam('obs') === "true") {
+    $('body').addClass('obs');
+    // 5min reload
+    setTimeout(function () {
+      location.reload();
+      console.log('reload')
+    }, 300000);
+  }
 });
