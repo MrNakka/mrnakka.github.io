581b36d1a1980ff0aca387755dbe5b9843293e9b
Nakka <waste.of.talent.nakka@gmail.com>
2025-03-15 19:41:55 +0900

    [update][add]マップの読み込みシステム追加、メンバー情報読み込み構築準備

--- tenichi_rust/js/script.js
+++ tenichi_rust/js/script.js
@@ -1,12 +1,71 @@
 $(function () {
-  $('.test1to2').on('click', function () {
-    // $('.team01').addClass('team02').css({
-    //   opacity: .5
-    // });
-    $('.team01').addClass('team02');
-    $('.team01').removeClass('team01');
-    // $('.team02').fadeIn(1000);
-  })
+  // load data
+  // member data load
+  var memberCsv = '/tenichi_rust/db/whitelist.csv';
+  function getMem(url) {
+    //CSVファイルを文字列で取得。
+    var txt = new XMLHttpRequest();
+    txt.open('get', memberCsv, false);
+    txt.send();
+    var arr = txt.responseText.split('\n');
+    var res = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == '') break;
+      res[i] = arr[i].replace(/\r\n|\r|\n/g, '').split(',');
+    }
+    return res;
+  }
+  // console.table(getMem())
+  // console.log(getMem().length)
+  // string
+  // console.log(getMem()[1][0])
+
+  // prefectures data load
+  var prefCsv = '/tenichi_rust/db/prefectures.csv';
+  function getPref(url) {
+    //CSVファイルを文字列で取得。
+    var txt = new XMLHttpRequest();
+    txt.open('get', prefCsv, false);
+    txt.send();
+    var arr = txt.responseText.split('\n');
+    var res = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == '') break;
+      res[i] = arr[i].replace(/\r\n|\r|\n/g, '').split(',');
+    }
+    return res;
+  }
+  
+  for (var i = 1; i < getPref().length; i++){
+    console.log('ClanTag '+ getPref()[i][2])
+    $('[data-code="' + i + '"]').addClass(getPref()[i][2]);
+  }
+  // console.table(getPref())
+  // console.log(getPref().length)
+  // string
+  // console.log(getPref()[1][0])
+
+  // 5mim load
+  // var clanJson = '/tenichi_rust/db/ClansList.json';
+  // $.getJSON(clanJson, function (clansData) {
+  //   console.log(clansData)
+  //   // steam id
+  //   // console.log(clansData[0].Members[0])
+  //   // string
+  //   console.log(String(clansData[0].Members[0]))
+  //   var steamidClan
+  //   // var stemidMem = $.inArray(String(clansData[0].Members[0]), getCsv())
+
+  //   // 0 ~ 121
+  //   // $.each(getCsv(), function (key, value) {
+  //   //   console.log(value.match(String(clansData[0].Members[0])))
+  //   //   // if () {
+  //   //   //   console.log('aaaaa'+key);
+  //   //   // }
+  //   // });
+  // });
+
+
 
   // param for obs
   function getParam(name, url) {
